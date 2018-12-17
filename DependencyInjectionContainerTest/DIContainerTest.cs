a1e8bfd70593f13df7b53a3724511cc43669de09
DanilaMaher <daniilnews@gmail.com>
2018-12-18 00:26:45 +0300

    DI container test project added

---
+++ DependencyInjectionContainerTest/DIContainerTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DependencyInjectionContainerTest
+{
+    [TestClass]
+    public class DIContainerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
