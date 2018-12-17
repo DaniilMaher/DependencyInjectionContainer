9e237710c7fa83de040274dad3f06705bc2f207d
DanilaMaher <daniilnews@gmail.com>
2018-12-18 02:42:52 +0300

    DependencyConfiguration implemented

--- DependencyInjectionContainer/Impl/DependenciesConfiguration.cs
+++ DependencyInjectionContainer/Impl/DependenciesConfiguration.cs
@@ -1,27 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DependencyInjectionContainer
 {
-    class DependencyConfiguration : IDependencyConfiguration
+    class DependencyConfiguration
     {
-        public bool IsSingletonDependency
+        private List<Dependency> dependencies;
+
+        public DependencyConfiguration()
         {
-            get;
-            private set;
+            dependencies = new List<Dependency>();
         }
 
-        public void Register(Type dependency, Type implementation)
+        public void Register(Type tDependency, Type tImplementation, bool isSingleton)
         {
-            throw new NotImplementedException();
+            if (tImplementation.IsInterface)
+            {
+                throw new Exception($"Can't register dependency {nameof(tDependency)} -> {nameof(tImplementation)}, {nameof(tImplementation)} is interface");
+            }
+            if (tImplementation.IsAbstract)
+            {
+                throw new Exception($"Can't register dependency {nameof(tDependency)} -> {nameof(tImplementation)}, {nameof(tImplementation)} is abstract");
+            }
+            if (!tDependency.IsAssignableFrom(tImplementation)) 
+            {
+                throw new Exception($"Can't register dependency {nameof(tDependency)} -> {nameof(tImplementation)}, {nameof(tDependency)} shoud be assignable from {nameof(tImplementation)}");
+            }
+
+            Dependency dependency = dependencies.Find(d => (d.TDependency == tDependency && d.TImplementation == tImplementation));
+            if (dependency == null)
+            {
+                dependency = new Dependency(tDependency, tImplementation, isSingleton);
+                dependencies.Add(dependency);
+            }
+            else
+            {
+                throw new Exception($"Can't register dependency {nameof(tDependency)} -> {nameof(tImplementation)}, dependency already registered");
+            }
         }
 
-        public void Register<TDependency, TImplementation>() where TImplementation : class, TDependency
+        public void Register<TDependency, TImplementation>(bool isSingleton) where TImplementation : class, TDependency
         {
-            throw new NotImplementedException();
+            Register(typeof(TDependency), typeof(TImplementation), isSingleton);
         }
     }
 }
